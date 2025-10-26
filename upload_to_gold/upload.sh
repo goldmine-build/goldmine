2f5a4612021c31047d8564f4f2b5422c3ed85172
Joe Gregorio <joe@bitworking.org>
2025-10-26 11:20:53 -0400

    Clean up comments.

--- upload_to_gold/upload.sh
+++ upload_to_gold/upload.sh
@@ -5,7 +5,7 @@ set -o pipefail
 
 bazel test //golden/modules/... //perf/modules/...
 
-bazel run //gold-client/cmd/goldctl -- auth --service-account ~/Downloads/api-project-146332866891-3816d0d33259.json --work-dir $HOME/goldctl
+bazel run //gold-client/cmd/goldctl -- auth --service-account ~/Downloads/api-project-146332866891-3816d0d33259.json --work-dir $HOME/goldctlx`
 
 WORKDIR=$HOME/goldctl
 EXTRACT_DIR=/tmp/gold
@@ -24,9 +24,13 @@ bazel run //gold-client/cmd/goldctl -- imgtest init \
 
 bazel run //puppeteer-tests/bazel/extract_puppeteer_screenshots -- --output_dir=$EXTRACT_DIR
 
-# Loop over all files in the "to_upload" directory and move them to the "gold" directory.
+# Loop over all files in the "EXTRACT_DIR" directory and `imgtest add` them,
+# which uploads the individual images to the cloud storage bucket, and also
+# updates the local metadata in the WORKDIR.
 for filename in $EXTRACT_DIR/*.png; do
   bazel run //gold-client/cmd/goldctl -- imgtest add --png-file $filename --test-name `echo $(basename $filename) | sed s/.*_// | sed s/.png//` --work-dir $WORKDIR
 done
 
+# Finalize the upload by uploading the metadata in the WORKDIR to the cloud
+# storage bucket.
 bazel run //gold-client/cmd/goldctl -- imgtest finalize --work-dir $HOME/goldctl
