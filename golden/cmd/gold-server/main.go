14eea14795d78f139eb25314199d81d2e90a2ef3
Joe Gregorio <joe@bitworking.org>
2025-12-13 20:12:36 -0500

    Adding services one by one.

--- golden/cmd/gold-server/main.go
+++ golden/cmd/gold-server/main.go
@@ -3,11 +3,16 @@ package main
 import (
 	"encoding/json"
 	"flag"
+	"net/http"
 
 	"go.goldmine.build/go/common"
+	"go.goldmine.build/go/httputils"
+	"go.goldmine.build/go/profsrv"
 	"go.goldmine.build/go/sklog"
+	"go.goldmine.build/golden/cmd/gold_ingestion/impl"
 	"go.goldmine.build/golden/go/config"
 	"go.goldmine.build/golden/go/services"
+	"golang.org/x/net/context"
 )
 
 func main() {
@@ -16,7 +21,9 @@ func main() {
 		configPath   = flag.String("config", "", "Path to the json5 file containing the instance configuration.")
 		servicesFlag = common.NewMultiStringFlag("services", nil, "The list of services to run. If not provided then all services wil be run.")
 		hang         = flag.Bool("hang", false, "Stop and do nothing after reading the flags. Good for debugging containers.")
-		promPort     = flag.String("prom_port", ":20000", "Metrics service address (e.g., ':10110')")
+		promPort     = flag.String("prom_port", ":20000", "Metrics service address (e.g., ':20000')")
+		pprofPort    = flag.String("pprof_port", "", "PProf handler (e.g., ':9001'). PProf not enabled if the empty string (default).")
+		healthzPort  = flag.String("healthz", ":10000", "Port that handles the healthz endpoint.")
 	)
 
 	common.InitWithMust(
@@ -24,7 +31,7 @@ func main() {
 		common.PrometheusOpt(promPort),
 	)
 
-	services, err := services.Validate(*servicesFlag)
+	activeServices, err := services.Validate(*servicesFlag)
 	if err != nil {
 		sklog.Fatal(err)
 	}
@@ -33,6 +40,8 @@ func main() {
 		sklog.Info("Hanging")
 		select {}
 	}
+
+	ctx := context.Background()
 
 	// Load the config file.
 	cfg, err := config.LoadConfigFromJSON5(*configPath)
@@ -46,8 +55,21 @@ func main() {
 	}
 	sklog.Infof("Loaded config\n %s", cfgAsJSON)
 
-	for _, s := range services {
+	// Start pprof services.
+	profsrv.Start(*pprofPort)
+
+	// Each service will sklog.Fatal if they fail, so we don't need extra error
+	// handling here.
+	for _, s := range activeServices {
 		// Start each service.
 		sklog.Infof("Starting service: %q", s)
+		switch s {
+		case services.Ingester:
+			go impl.IngestionMain(ctx, cfg)
+		}
 	}
+
+	// Handle healthz.
+	http.HandleFunc("/healthz", httputils.ReadyHandleFunc)
+	sklog.Fatal(http.ListenAndServe(*healthzPort, nil))
 }
