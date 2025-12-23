dea669e4e251d3687d6f742143145782327b538c
Joe Gregorio <joe@bitworking.org>
2025-12-22 22:18:58 -0500

    Make db.MustInitSQLDatabase on create one pool and then use that for all subsequent calls.

--- golden/go/db/db.go
+++ golden/go/db/db.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"context"
+	"sync"
 
 	"github.com/jackc/pgx/v4/pgxpool"
 	"go.goldmine.build/go/sklog"
@@ -14,7 +15,19 @@ const (
 	maxSQLConnections = 20
 )
 
+var (
+	db     *pgxpool.Pool = nil
+	dbOnce sync.Once
+)
+
 func MustInitSQLDatabase(ctx context.Context, cfg config.Common) *pgxpool.Pool {
+	dbOnce.Do(func() {
+		db = mustInitSQLDatabaseImpl(ctx, cfg)
+	})
+	return db
+}
+
+func mustInitSQLDatabaseImpl(ctx context.Context, cfg config.Common) *pgxpool.Pool {
 	if cfg.SQLDatabaseName == "" {
 		sklog.Fatalf("Must have SQL Database Information")
 	}
