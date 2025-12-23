b31272e8ea18774c5b5f8e32f8f7ce36377f6b15
Joe Gregorio <joe@bitworking.org>
2025-12-23 16:27:48 -0500

    Clean up diffcalculator.go of unneeded constants.

--- golden/cmd/diffcalculator/diffcalculator.go
+++ golden/cmd/diffcalculator/diffcalculator.go
@@ -5,28 +5,11 @@ package main
 
 import (
 	"context"
-	"time"
 
 	"go.goldmine.build/go/common"
 	"go.goldmine.build/go/sklog"
 	"go.goldmine.build/golden/cmd/diffcalculator/impl"
 	"go.goldmine.build/golden/go/config"
-)
-
-const (
-	// An arbitrary amount.
-	maxSQLConnections = 20
-
-	// The GCS folder that contains the images, named by their digests.
-	imgFolder = "dm-images-v1"
-
-	calculateCLDataProportion = 0.8
-
-	primaryBranchStalenessThreshold = time.Minute
-
-	diffCalculationTimeout = 10 * time.Minute
-
-	groupingCacheSize = 100_000
 )
 
 var flags config.ServerFlags
