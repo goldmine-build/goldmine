cbdca132f98efb34a51e4b8b0a1f0884b9b7f451
Joe Gregorio <joe@bitworking.org>
2026-01-19 11:30:11 -0500

    cluster-page-sk supports darkmode

--- golden/modules/cluster-page-sk/cluster-page-sk_puppeteer_test.ts
+++ golden/modules/cluster-page-sk/cluster-page-sk_puppeteer_test.ts
@@ -4,11 +4,13 @@ import {
   addEventListenersToPuppeteerPage,
   EventName,
   loadCachedTestBed,
-  takeScreenshot,
+  ModeOption,
+  Modes,
+  takeScreenshotWithMode,
   TestBed,
 } from '../../../puppeteer-tests/util';
-import { positiveDigest, negativeDigest, untriagedDigest } from './test_data';
 import { ClusterPageSkPO } from './cluster-page-sk_po';
+import { negativeDigest, positiveDigest, untriagedDigest } from './test_data';
 
 describe('cluster-page-sk', () => {
   let testBed: TestBed;
@@ -43,91 +45,123 @@ describe('cluster-page-sk', () => {
     expect(await testBed.page.$$('cluster-page-sk')).to.have.length(1);
   });
 
-  it('should take a screenshot', async () => {
-    await takeScreenshot(testBed.page, 'gold', 'cluster-page-sk');
-  });
-
-  it('shows details about a single digest when clicked', async () => {
-    await clusterPageSkPO.clusterDigestsSkPO.clickNode(positiveDigest);
-    await takeScreenshot(
-      testBed.page,
-      'gold',
-      'cluster-page-sk_one-digest-selected'
-    );
-  });
-
-  it('shows diff between two digests that are selected', async () => {
-    await clusterPageSkPO.clusterDigestsSkPO.clickNode(positiveDigest);
-    await clusterPageSkPO.clusterDigestsSkPO.shiftClickNode(negativeDigest);
-    await takeScreenshot(
-      testBed.page,
-      'gold',
-      'cluster-page-sk_two-digests-selected'
-    );
-  });
-
-  it('shows a summary when more than two digests are selected', async () => {
-    await clusterPageSkPO.clusterDigestsSkPO.clickNode(positiveDigest);
-    await clusterPageSkPO.clusterDigestsSkPO.shiftClickNode(negativeDigest);
-    await clusterPageSkPO.clusterDigestsSkPO.shiftClickNode(untriagedDigest);
-    await takeScreenshot(
-      testBed.page,
-      'gold',
-      'cluster-page-sk_three-digests-selected'
-    );
-  });
-
-  it('shows all values when a paramset key is clicked', async () => {
-    const done = promiseFactory('layout-complete');
-    await clusterPageSkPO.paramSetSkPO.clickKey('gpu');
-    await done;
-    await takeScreenshot(testBed.page, 'gold', 'cluster-page-sk_key-clicked');
-  });
-
-  it('shows nodes with matching values when a value is clicked', async () => {
-    const done = promiseFactory('layout-complete');
-    await clusterPageSkPO.paramSetSkPO.clickValue({
-      paramSetIndex: 0,
-      key: 'gpu',
-      value: 'AMD',
+  Modes.forEach(async (mode: ModeOption) => {
+    it('should take a screenshot', async () => {
+      await takeScreenshotWithMode(
+        testBed.page,
+        'gold',
+        'cluster-page-sk',
+        mode
+      );
     });
-    await done;
-    await takeScreenshot(testBed.page, 'gold', 'cluster-page-sk_value-clicked');
-  });
 
-  it('can zoom in using the keyboard', async () => {
-    const done = promiseFactory('layout-complete');
-    await clusterPageSk.type('aa');
-    await done;
-    await takeScreenshot(testBed.page, 'gold', 'cluster-page-sk_zoom-in');
-  });
+    it('shows details about a single digest when clicked', async () => {
+      await clusterPageSkPO.clusterDigestsSkPO.clickNode(positiveDigest);
+      await takeScreenshotWithMode(
+        testBed.page,
+        'gold',
+        'cluster-page-sk_one-digest-selected',
+        mode
+      );
+    });
 
-  it('can zoom out using the keyboard', async () => {
-    const done = promiseFactory('layout-complete');
-    await clusterPageSk.type('zz');
-    await done;
-    await takeScreenshot(testBed.page, 'gold', 'cluster-page-sk_zoom-out');
-  });
+    it('shows diff between two digests that are selected', async () => {
+      await clusterPageSkPO.clusterDigestsSkPO.clickNode(positiveDigest);
+      await clusterPageSkPO.clusterDigestsSkPO.shiftClickNode(negativeDigest);
+      await takeScreenshotWithMode(
+        testBed.page,
+        'gold',
+        'cluster-page-sk_two-digests-selected',
+        mode
+      );
+    });
 
-  it('can increase node spacing using the keyboard', async () => {
-    const done = promiseFactory('layout-complete');
-    await clusterPageSk.type('ss');
-    await done;
-    await takeScreenshot(
-      testBed.page,
-      'gold',
-      'cluster-page-sk_more-node-space'
-    );
-  });
+    it('shows a summary when more than two digests are selected', async () => {
+      await clusterPageSkPO.clusterDigestsSkPO.clickNode(positiveDigest);
+      await clusterPageSkPO.clusterDigestsSkPO.shiftClickNode(negativeDigest);
+      await clusterPageSkPO.clusterDigestsSkPO.shiftClickNode(untriagedDigest);
+      await takeScreenshotWithMode(
+        testBed.page,
+        'gold',
+        'cluster-page-sk_three-digests-selected',
+        mode
+      );
+    });
 
-  it('can decrease node spacing using the keyboard', async () => {
-    const done = promiseFactory('layout-complete');
-    await clusterPageSk.type('xx');
-    await done;
-    await takeScreenshot(
-      testBed.page,
-      'gold',
-      'cluster-page-sk_less-node-space'
-    );
+    it('shows all values when a paramset key is clicked', async () => {
+      const done = promiseFactory('layout-complete');
+      await clusterPageSkPO.paramSetSkPO.clickKey('gpu');
+      await done;
+      await takeScreenshotWithMode(
+        testBed.page,
+        'gold',
+        'cluster-page-sk_key-clicked',
+        mode
+      );
+    });
+
+    it('shows nodes with matching values when a value is clicked', async () => {
+      const done = promiseFactory('layout-complete');
+      await clusterPageSkPO.paramSetSkPO.clickValue({
+        paramSetIndex: 0,
+        key: 'gpu',
+        value: 'AMD',
+      });
+      await done;
+      await takeScreenshotWithMode(
+        testBed.page,
+        'gold',
+        'cluster-page-sk_value-clicked',
+        mode
+      );
+    });
+
+    it('can zoom in using the keyboard', async () => {
+      const done = promiseFactory('layout-complete');
+      await clusterPageSk.type('aa');
+      await done;
+      await takeScreenshotWithMode(
+        testBed.page,
+        'gold',
+        'cluster-page-sk_zoom-in',
+        mode
+      );
+    });
+
+    it('can zoom out using the keyboard', async () => {
+      const done = promiseFactory('layout-complete');
+      await clusterPageSk.type('zz');
+      await done;
+      await takeScreenshotWithMode(
+        testBed.page,
+        'gold',
+        'cluster-page-sk_zoom-out',
+        mode
+      );
+    });
+
+    it('can increase node spacing using the keyboard', async () => {
+      const done = promiseFactory('layout-complete');
+      await clusterPageSk.type('ss');
+      await done;
+      await takeScreenshotWithMode(
+        testBed.page,
+        'gold',
+        'cluster-page-sk_more-node-space',
+        mode
+      );
+    });
+
+    it('can decrease node spacing using the keyboard', async () => {
+      const done = promiseFactory('layout-complete');
+      await clusterPageSk.type('xx');
+      await done;
+      await takeScreenshotWithMode(
+        testBed.page,
+        'gold',
+        'cluster-page-sk_less-node-space',
+        mode
+      );
+    });
   });
 });
