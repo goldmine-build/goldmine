49b7bd71ed76b58c7b546f91b1a43403ead37639
Joe Gregorio <joe@bitworking.org>
2026-01-19 22:58:34 -0500

    ignores-page-sk supports darkmode. The light mode delete dialog captured with the darkmode page is an artifact of dialog element and how takeScreenshotWithMode works.

--- golden/modules/ignores-page-sk/ignores-page-sk_puppeteer_test.ts
+++ golden/modules/ignores-page-sk/ignores-page-sk_puppeteer_test.ts
@@ -3,7 +3,9 @@ import { Page } from 'puppeteer';
 import {
   addEventListenersToPuppeteerPage,
   loadCachedTestBed,
-  takeScreenshot,
+  ModeOption,
+  Modes,
+  takeScreenshotWithMode,
   TestBed,
 } from '../../../puppeteer-tests/util';
 
@@ -21,56 +23,71 @@ describe('ignores-page-sk', () => {
   });
 
   describe('screenshots', () => {
-    it('should show the default page', async () => {
-      await navigateTo(testBed.page, testBed.baseUrl);
-      await testBed.page.setViewport({ width: 1300, height: 2100 });
-      await takeScreenshot(testBed.page, 'gold', 'ignores-page-sk');
-    });
+    Modes.forEach(async (mode: ModeOption) => {
+      it('should show the default page', async () => {
+        await navigateTo(testBed.page, testBed.baseUrl);
+        await testBed.page.setViewport({ width: 1300, height: 2100 });
+        await takeScreenshotWithMode(
+          testBed.page,
+          'gold',
+          'ignores-page-sk',
+          mode
+        );
+      });
 
-    it('should show the counts of all traces', async () => {
-      await navigateTo(testBed.page, testBed.baseUrl, '?count_all=true');
-      await testBed.page.setViewport({ width: 1300, height: 2100 });
-      await takeScreenshot(testBed.page, 'gold', 'ignores-page-sk_all-traces');
-    });
+      it('should show the counts of all traces', async () => {
+        await navigateTo(testBed.page, testBed.baseUrl, '?count_all=true');
+        await testBed.page.setViewport({ width: 1300, height: 2100 });
+        await takeScreenshotWithMode(
+          testBed.page,
+          'gold',
+          'ignores-page-sk_all-traces',
+          mode
+        );
+      });
 
-    it('should show the delete confirmation dialog', async () => {
-      await navigateTo(testBed.page, testBed.baseUrl);
-      // Focus in a little to see better.
-      await testBed.page.setViewport({ width: 1300, height: 1300 });
-      await testBed.page.click(
-        'ignores-page-sk tbody > tr:nth-child(1) > td.mutate-icons > delete-icon-sk'
-      );
-      await takeScreenshot(
-        testBed.page,
-        'gold',
-        'ignores-page-sk_delete-dialog'
-      );
-    });
+      it('should show the delete confirmation dialog', async () => {
+        await navigateTo(testBed.page, testBed.baseUrl);
+        // Focus in a little to see better.
+        await testBed.page.setViewport({ width: 1300, height: 1300 });
+        await testBed.page.click(
+          'ignores-page-sk tbody > tr:nth-child(1) > td.mutate-icons > delete-icon-sk'
+        );
+        await takeScreenshotWithMode(
+          testBed.page,
+          'gold',
+          'ignores-page-sk_delete-dialog',
+          mode
+        );
+      });
 
-    it('should show the edit ignore rule modal when update is clicked', async () => {
-      await navigateTo(testBed.page, testBed.baseUrl);
-      // Focus in a little to see better.
-      await testBed.page.setViewport({ width: 1300, height: 1300 });
-      await testBed.page.click(
-        'ignores-page-sk tbody > tr:nth-child(5) > td.mutate-icons > mode-edit-icon-sk'
-      );
-      await takeScreenshot(
-        testBed.page,
-        'gold',
-        'ignores-page-sk_update-modal'
-      );
-    });
+      it('should show the edit ignore rule modal when update is clicked', async () => {
+        await navigateTo(testBed.page, testBed.baseUrl);
+        // Focus in a little to see better.
+        await testBed.page.setViewport({ width: 1300, height: 1300 });
+        await testBed.page.click(
+          'ignores-page-sk tbody > tr:nth-child(5) > td.mutate-icons > mode-edit-icon-sk'
+        );
+        await takeScreenshotWithMode(
+          testBed.page,
+          'gold',
+          'ignores-page-sk_update-modal',
+          mode
+        );
+      });
 
-    it('should show the create ignore rule modal when create is clicked', async () => {
-      await navigateTo(testBed.page, testBed.baseUrl);
-      // Focus in a little to see better.
-      await testBed.page.setViewport({ width: 1300, height: 1300 });
-      await testBed.page.click('ignores-page-sk .controls button.create');
-      await takeScreenshot(
-        testBed.page,
-        'gold',
-        'ignores-page-sk_create-modal'
-      );
+      it('should show the create ignore rule modal when create is clicked', async () => {
+        await navigateTo(testBed.page, testBed.baseUrl);
+        // Focus in a little to see better.
+        await testBed.page.setViewport({ width: 1300, height: 1300 });
+        await testBed.page.click('ignores-page-sk .controls button.create');
+        await takeScreenshotWithMode(
+          testBed.page,
+          'gold',
+          'ignores-page-sk_create-modal',
+          mode
+        );
+      });
     });
   });
 });
